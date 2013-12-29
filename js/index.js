34b72483c3fa8143027d9705e5df44c82f0d843a
Jeremy Walker <jez.walker@gmail.com>
2013-12-29 23:39:19 +0000

    Add real mnemonics

--- js/index.js
+++ js/index.js
@@ -2,51 +2,24 @@ var grid2 = ["a","b"];
 var grid3 = ["a","b","c"];
 var grid4 = ["a","b","c","d"];
 
-var MNEMONICS = [
-  {id: 1, topic_id: 0, subject:"Bowel components", mnemonic: "<strong>D</strong>ow <strong>J</strong>ones <strong>I</strong>ndustrial <strong>A</strong>verage <strong>C</strong>losing <strong>S</strong>tock <strong>R</strong>eport", image_src: "http://equityclock.com/pictures/SeasonalChartsDowJonesIndustrialAverageS_269F/image_4.png"},
-  {id: 2, topic_id: 0, subject:"Atrioventricular valves Hi Yield", mnemonic: "LAB RAT", image_src: "http://cardiffstudentmedia.co.uk/gairrhydd/wp-content/uploads/sites/2/2013/11/mouse-lab-rat-101117-02.jpg"},
-  {id: 3, topic_id: 0, subject:"Axillary artery branches", mnemonic: "Screw The Lawyer Save A Patient", image_src: "http://ballinyourcourt.files.wordpress.com/2012/10/screw-you.jpg"},
-  {id: 4, topic_id: 0, subject: "Brachial artery: recurrent and collateral branches", mnemonic: "I Am Pretty Sexy", image_src: "http://gagmark.com/wp-content/uploads/2012/10/Im-Sexy.png"}
-];
-
-var TOPICS = [
-  {id: 0, name: "Anatomy"},
-  {id: 1, name: "Cardiology"},
-  {id: 2, name: "Anesthesiology", mnemonics: []},
-  {id: 2, name: "Psychology", mnemonics: []},
-  {id: 2, name: "Biochemistry", mnemonics: []},
-  {id: 3, name: "Cardiology", mnemonics: []},
-  {id: 3, name: "Chemistry", mnemonics: []},
-  {id: 3, name: "Dermatology", mnemonics: []},
-  {id: 3, name: "Embryrology", mnemonics: []},
-  {id: 3, name: "Emergency Medicine", mnemonics: []},
-  {id: 3, name: "ENT", mnemonics: []},
-  {id: 3, name: "Epidemiology", mnemonics: []},
-  {id: 3, name: "Gastroenterology", mnemonics: []},
-  {id: 3, name: "Genetics", mnemonics: []},
-  {id: 3, name: "Histology", mnemonics: []},
-  {id: 3, name: "Immunology", mnemonics: []},
-  {id: 3, name: "General Practice", mnemonics: []},
-  {id: 3, name: "Physical Exam", mnemonics: []},
-  {id: 3, name: "Microbiology", mnemonics: []},
-  {id: 3, name: "Neurology", mnemonics: []},
-  {id: 3, name: "Neurosciences", mnemonics: []},
-  {id: 3, name: "Obstetrics / Gynecology", mnemonics: []},
-  {id: 3, name: "Ophthamology", mnemonics: []},
-  {id: 3, name: "Orthopedics", mnemonics: []},
-  {id: 3, name: "Pathology", mnemonics: []},
-  {id: 3, name: "Pediatrics", mnemonics: []},
-  {id: 3, name: "Pharmacology", mnemonics: []},
-  {id: 3, name: "Physiology", mnemonics: []},
-  {id: 3, name: "Psychiatry", mnemonics: []},
-  {id: 3, name: "Oncology", mnemonics: []},
-  {id: 3, name: "Rheumatology", mnemonics: []},
-  {id: 3, name: "Surgery", mnemonics: []},
-  {id: 3, name: "Urology", mnemonics: []}
-]
-
+var TOPICS;
 
 var $mnemonic;
+
+function setupTopics() {
+  var topics = {}
+  for(i in MNEMONICS) {
+    topics[MNEMONICS[i].topic] = true;
+  }
+
+  window.TOPICS = []
+  id = 1
+  for(name in topics) {
+    window.TOPICS.push({id: id, name: name});
+    id += 1;
+  }
+}
+setupTopics();
 
 $(function() {
   $mnemonic = $('#mnemonic');
@@ -73,7 +46,7 @@ var app = {
     renderTopics();
   },
 };
-//
+
 $(document).bind( "pagebeforechange", function( e, data ) {
 
   if ( typeof data.toPage === "string" ) {
@@ -140,12 +113,12 @@ function renderTopic(topic)
   $page = $('#topic')
   $page.find('h1').html(topic.name);
 
-  var mnemonics = $.grep(MNEMONICS, function(e,i){return e.topic_id == topic.id});
+  var mnemonics = $.grep(MNEMONICS, function(e,i){return e.topic == topic.name});
   $page.find('.mnenomics').html("");
   for(i in mnemonics) {
     mnemonic = mnemonics[i];
     css_class = "mnemonic box ui-block-" + (grid2[i%2]);
-    mnemonic_html = "<a href='#mnemonic?id=" + mnemonic.id + "' class='" + css_class + "'><div class='img' style='background-image:url(" + mnemonic.image_src + ")'/><div class='subject'>" + mnemonic.subject + "</div><div class='mnemonic-string'>" + mnemonic.mnemonic + "</div></a>"
+    mnemonic_html = "<a href='#mnemonic?id=" + mnemonic.id + "' class='" + css_class + "'><div class='img' style='background-image:url(" + mnemonic.image_urls[0] + ")'/><div class='subject'>" + mnemonic.subject + "</div><div class='mnemonic-string'>" + mnemonic.mnemonic + "</div></a>"
     $page.find('.mnenomics').append(mnemonic_html);
   }
 }
@@ -155,7 +128,7 @@ function renderMnemonic(mnemonic)
   topic = $.grep(TOPICS, function(e,i) {return e.id == mnemonic.topic_id})[0];
 
   $mnemonic.mnemonic = mnemonic;
-  $mnemonic.find('.header').css('backgroundImage', "url(" + mnemonic.image_src + ")");
+  $mnemonic.find('.header').css('backgroundImage', "url(" + mnemonic.image_urls[0] + ")");
   $mnemonic.find('h1').html(mnemonic.mnemonic);
   $mnemonic.find('.subject').html(mnemonic.subject);
   $mnemonic.find('.mnemonic').html(mnemonic.mnemonic);
